d3fc1ccbb25fea7ea22134f5aff39004a83d1026
Paulo Ricardo <prsousa.128@gmail.com>
2025-03-01 17:42:10 -0300

    💄style: update style hero component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,23 +1,9 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Contact() {
   return (
     <section id="contact" className="p-8 text-center">
       <h3 className="text-3xl font-bold">Contato</h3>
       <p className="mt-4">Entre em contato pelo e-mail: prsousa.dev@gmail.com</p>
-      
-      {/* Redes Sociais */}
-      <div className="flex justify-center gap-6 mt-6 text-2xl">
-        <a href="https://github.com/prsousa8" target="_blank" rel="noopener noreferrer" className="hover:text-gray-600">
-          <FaGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/pauloricardo8/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
-          <FaLinkedin />
-        </a>
-        <a href="https://www.instagram.com/pauloricardo_176/" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
-          <FaInstagram />
-        </a>
-      </div>
     </section>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 // components/Hero.tsx
 import Image from "next/image";
 import image from "../../public/foto.svg";
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 export default function Hero() {
   return (
@@ -13,11 +14,23 @@ export default function Hero() {
           objectFit="contain"
         />
       </div>
-      <h2 className="text-4xl font-bold z-10 mt-4">Olá, sou o Paulo Ricardo</h2>
-      <p className="text-lg mt-2 z-10">Desenvolvedor Front-end especializado em React e Next.js</p>
+      <h2 className="text-4xl font-bold z-10 mt-4 mx-2 text-center">Olá, sou o Paulo Ricardo</h2>
+      <p className="text-lg mt-2 z-10 mx-2 text-center">Desenvolvedor Front-end especializado em React e Next.js</p>
       
       {/* Gradiente na parte inferior */}
       <div className="absolute bottom-0 left-0 w-full h-32 bg-gradient-to-t from-blue-900 to-transparent"></div>
+      {/* Redes Sociais */}
+      <div className="flex justify-center gap-6 mt-6 text-2xl">
+        <a href="https://github.com/prsousa8" target="_blank" rel="noopener noreferrer" className="hover:text-gray-600">
+          <FaGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/pauloricardo8/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.instagram.com/pauloricardo_176/" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <FaInstagram />
+        </a>
+      </div>
     </section>
   );
 }
